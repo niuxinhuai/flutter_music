b295e03f5d691cc32cbd0b20a83429f9c949d9ee
niuxinhuai <niuxinhuai@super-niu-2.local>
2022-08-03 17:06:57 +0800

    helper

---
+++ lib/helper/router.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_music/helper/router_helper.dart';
+
+class ARouter {
+  static Future<dynamic> open(BuildContext context, String url,
+      {Map<String, dynamic>? params}) {
+    return Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      return RouterHelper.routers[url]!.buildPage(params);
+    })).then((value) => value);
+  }
+
+  static close(BuildContext context, {Map<String, dynamic>? result}) {
+    return Navigator.pop(context, result);
+  }
+
+  static closeDelay(BuildContext context, Duration dutation) {
+    return Future.delayed(dutation, () {
+      return Navigator.pop(context);
+    });
+  }
+}
---
+++ lib/helper/service_helper.dart
@@ -0,0 +1,81 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_music/constants/url.dart';
+//import 'package:flutter_wanandroid/helper/user.dart';
+
+class ServiceHelper {
+  static final Dio _dio = Dio(BaseOptions(
+      baseUrl: UrlConstants.baseUrl,
+      connectTimeout: 30000,
+      receiveTimeout: 30000,
+      contentType: "application/x-www-form-urlencoded; charset=utf-8"));
+
+  static Future<T> post<T>(
+    String path, {
+    data,
+    Options? options,
+    CancelToken? cancelToken,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) {
+    return _dio
+        .post<T>(path,
+            options: options ?? _getOptions(),
+            data: data,
+            cancelToken: cancelToken,
+            onSendProgress: onSendProgress,
+            onReceiveProgress: onReceiveProgress)
+        .then((value) => value.data!);
+  }
+
+  static Future<T> get<T>(
+    String path, {
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    ProgressCallback? onReceiveProgress,
+  }) {
+    return _dio
+        .get<T>(path,
+            queryParameters: queryParameters,
+            options: options ?? _getOptions(),
+            cancelToken: cancelToken,
+            onReceiveProgress: onReceiveProgress)
+        .then((value) => value.data!);
+  }
+
+  static Future<Response> download(
+    String? urlPath,
+    savePath, {
+    ProgressCallback? onReceiveProgress,
+    Map<String, dynamic>? queryParameters,
+    CancelToken? cancelToken,
+    lengthHeader = HttpHeaders.contentLengthHeader,
+    data,
+    Options? options,
+  }) {
+    return _dio.download(urlPath!, savePath,
+        onReceiveProgress: onReceiveProgress,
+        queryParameters: queryParameters,
+        cancelToken: cancelToken,
+        lengthHeader: lengthHeader,
+        data: data,
+        options: options);
+  }
+
+  static Future<T> upload<T>(String urlPath, Map<String, dynamic> data) {
+    var formData = FormData.fromMap(data);
+    return _dio.post<T>(urlPath, data: formData).then((value) => value.data!);
+  }
+
+  static Options? _getOptions() {
+    return null;
+//    Map<String, String>? map = UserDefault.getHeader();
+//    if (map != null) {
+//      return Options(headers: map);
+//    } else {
+//      return null;
+//    }
+  }
+}
