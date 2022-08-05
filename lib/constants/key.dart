ec83d53f691cb6d13f36dadb307db1978b0395f9
niuxinhuai <niuxinhuai@super-niu-2.local>
2022-08-05 17:28:01 +0800

    constants

---
+++ lib/constants/key.dart
@@ -0,0 +1,9 @@
+class ConstantsKey {
+  static final String encrypterKey =
+      "my 32 length key................"; //qwertyuiop1234567lkjhgf
+  static final String encrypterIV = "0123456789ABCDEF";
+
+  static final String weatherToken = "oU6JnqAWhL9mMHks";
+
+  static final String aMapiOSKey = "9ef9431c196f67437cfa403dfad4a969";
+}
--- lib/constants/url.dart
+++ lib/constants/url.dart
@@ -8,4 +8,42 @@ class MusicUri extends UrlConstants {
   ///可选参数 : refresh: 是否刷新数据,默认为 false
   static String home({bool refresh = false}) =>
       "/homepage/block/page?refresh=$refresh";
+
+  ///登录
+  static String phoneLogin(String phone, String password) =>
+      "/login/cellphone?phone=$phone&password=$password";
+
+  ///游客登录
+  static String visitorLogin = "/register/anonimous";
+
+  ///推荐音乐
+  static const String recommend_url = "/recommend/resource";
+
+  ///歌单分类
+  ///说明 : 调用此接口,可获取歌单分类,包含 category 信息
+  static const String catlist = "/playlist/catlist";
+
+  ///热门歌单分类
+  ///说明 : 调用此接口,可获取歌单分类,包含 category 信息
+  static const String hot = "/playlist/hot";
+
+  /**
+   * 歌单 ( 网友精选碟 )
+      说明 : 调用此接口 , 可获取网友精选碟歌单
+
+      可选参数 : order: 可选值为 'new' 和 'hot', 分别对应最新和最热 , 默认为 'hot'
+
+      cat: tag, 比如 " 华语 "、" 古风 " 、" 欧美 "、" 流行 ", 默认为 "全部",可从歌单分类接口获取(/playlist/catlist)
+
+      limit: 取出歌单数量 , 默认为 50
+
+      offset: 偏移数量 , 用于分页 , 如 :( 评论页数 -1)*50, 其中 50 为 limit 的值
+   *
+   * */
+  static String getSongPlayList(
+          {int limit = 10,
+          int offset = 0,
+          String order = "hot",
+          String tag = "全部"}) =>
+      "/top/playlist?limit=$limit&offset=$offset&order=$order&cat=$tag";
 }
