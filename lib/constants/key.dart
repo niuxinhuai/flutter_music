ebc93dfdb4419674190f2b8acf467128decbbea5
niuxinhuai <niuxinhuai@super-niu-2.local>
2022-08-16 11:19:22 +0800

    key

--- lib/constants/key.dart
+++ lib/constants/key.dart
@@ -6,4 +6,10 @@ class ConstantsKey {
   static final String weatherToken = "oU6JnqAWhL9mMHks";
 
   static final String aMapiOSKey = "9ef9431c196f67437cfa403dfad4a969";
+
+  //歌单广场，个人数据key
+  static final String squareKey = "-1";
+
+  //歌单广场，精品歌单默认显示
+  static final String square_tag = "全部精品";
 }
