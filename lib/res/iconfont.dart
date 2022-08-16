a4c1b822e3f9bc391f3c6a57bca779926cc597f0
niuxinhuai <niuxinhuai@super-niu-2.local>
2022-08-16 11:18:57 +0800

    iconfont

--- lib/res/iconfont.dart
+++ lib/res/iconfont.dart
@@ -1,26 +1,29 @@
 import 'package:flutter/widgets.dart' hide Action;
 
+
 // 代码由程序自动生成。请不要对此文件做任何修改。
 
+
 class IconF {
+
   IconF._();
 
   static const _font_name = 'iconfont';
 
-  static const IconData a_tupianyihuifu_05 =
-      const IconData(0xe604, fontFamily: _font_name);
-  static const IconData calendarrili =
-      const IconData(0xe729, fontFamily: _font_name);
+  static const IconData airdrop = const IconData(0xe927, fontFamily: _font_name);
+  static const IconData atupianyihuifu05 = const IconData(0xe604, fontFamily: _font_name);
+  static const IconData calendarrili = const IconData(0xe729, fontFamily: _font_name);
   static const IconData gedan = const IconData(0xe61b, fontFamily: _font_name);
-  static const IconData paihangbang =
-      const IconData(0xe782, fontFamily: _font_name);
+  static const IconData message = const IconData(0xe6e0, fontFamily: _font_name);
+  static const IconData music_menu = const IconData(0xe656, fontFamily: _font_name);
+  static const IconData next = const IconData(0xe628, fontFamily: _font_name);
+  static const IconData paihangbang = const IconData(0xe782, fontFamily: _font_name);
+  static const IconData prev = const IconData(0xe627, fontFamily: _font_name);
   static const IconData tushu = const IconData(0xe600, fontFamily: _font_name);
-  static const IconData wodeguanzhu =
-      const IconData(0xe616, fontFamily: _font_name);
-  static const IconData xianchangzhibo =
-      const IconData(0xe65b, fontFamily: _font_name);
-  static const IconData yinlemusic217 =
-      const IconData(0xe713, fontFamily: _font_name);
-  static const IconData zhuanji =
-      const IconData(0xe60a, fontFamily: _font_name);
+  static const IconData voice_none = const IconData(0xeca8, fontFamily: _font_name);
+  static const IconData wodeguanzhu = const IconData(0xe616, fontFamily: _font_name);
+  static const IconData xianchangzhibo = const IconData(0xe65b, fontFamily: _font_name);
+  static const IconData yinlemusic217 = const IconData(0xe713, fontFamily: _font_name);
+  static const IconData zhuanji = const IconData(0xe60a, fontFamily: _font_name);
+
 }
