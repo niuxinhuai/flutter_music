c4e2e8f740897bd53c2b3a27027476f5068fc272
niuxinhuai <niuxinhuai@super-niu-2.local>
2022-08-16 11:07:39 +0800

    router

--- lib/helper/router.dart
+++ lib/helper/router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_music/helper/router_helper.dart';
+import 'package:flutter_music/widgets/router.dart';
 
 class ARouter {
   static Future<dynamic> open(BuildContext context, String url,
@@ -9,6 +10,14 @@ class ARouter {
         .push(MaterialPageRoute(builder: (BuildContext context) {
       return RouterHelper.routers[url]!.buildPage(params);
     })).then((value) => value);
+  }
+
+  static Future<dynamic> present(BuildContext context, String url,
+      {Map<String, dynamic>? params}) {
+    return Navigator.of(context)
+        .push(
+            SlideTransitionRoute(RouterHelper.routers[url]!.buildPage(params)))
+        .then((value) => value);
   }
 
   static close(BuildContext context, {Map<String, dynamic>? result}) {
--- lib/helper/router_helper.dart
+++ lib/helper/router_helper.dart
@@ -1,4 +1,9 @@
 import 'package:fish_redux/fish_redux.dart' as FishRedux;
+import 'package:flutter_music/sections/login/page/login/page.dart';
+import 'package:flutter_music/sections/login/page/photo_login/page.dart';
+import 'package:flutter_music/sections/music/page/playlist_detail/page.dart';
+import 'package:flutter_music/sections/music/page/recommend_list/page.dart';
+import 'package:flutter_music/sections/music/page/song/page.dart';
 import 'package:flutter_music/sections/square/page/edit/page.dart';
 import 'package:flutter_music/sections/square/page/page.dart';
 
@@ -6,13 +11,18 @@ class RouterHelper {
   static Map<String, FishRedux.Page> routers = {
     RouterKeys.square: PlaylistSquarePage(),
     RouterKeys.square_edit: SquareEditPage(),
+    RouterKeys.login: LoginPage(),
+    RouterKeys.photo_login: PhotoLoginPage(),
+    RouterKeys.every_day_recommend_song: RecommendListPage(),
+    RouterKeys.song: AudioPlayerPage(),
+    RouterKeys.playlist_detail: PlaylistDetailPage()
   };
 }
 
 class RouterKeys {
   static const login = "login";
-  static const register = "register";
-  static const article = "article";
+  static const photo_login = "photo_login";
+  static const every_day_recommend_song = "every_day_recommend_song";
 
   //入口类
   static const applicaiton = "application";
@@ -22,4 +32,10 @@ class RouterKeys {
 
   //跟单category编辑
   static const square_edit = "square_edit";
+
+  //音乐播放界面
+  static const song = "song";
+
+  //歌单详情
+  static const playlist_detail = "play_list_detail";
 }
