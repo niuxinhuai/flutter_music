d95006956ddab26565ab07a1a530e55f8ccb8cb1
niuxinhuai <niuxinhuai@super-niu-2.local>
2022-08-16 11:07:19 +0800

    json key

---
+++ lib/constants/json.dart
@@ -0,0 +1,42 @@
+class JsonStringConstants {
+  ///获取每日推荐歌曲
+  static const String recommend_songs = "assets/json/recommend_songs.json";
+
+  ///获取每日推荐歌单
+  static const String recommend_resource =
+      "assets/json/recommend_resource.json";
+
+  ///登录数据
+  static const String email_login = "assets/json/email_login.json";
+
+  ///歌单广场
+  static const String square_all = "assets/json/square_all.json";
+
+  ///广场全部分类
+  static const String catlist_all = "assets/json/catlist_all.json";
+
+  ///广场热门分类
+  static const String hot_catlist = "assets/json/hot_catlist.json";
+
+  ///广场精品歌单
+  static const String highquality_tags = "assets/json/highquality_tags.json";
+
+  ///广场精品歌单标签列表
+  static const String highquality_list = "assets/json/highquality_list.json";
+
+  ///音乐URL
+  static const String song_url = "assets/json/song_url.json";
+
+  ///音乐对应的歌词
+  static const String song_lyric = "assets/json/song_lyric.json";
+
+  ///发现页数据
+  static const String discorver_block_page =
+      "assets/json/discorver_block_page.json";
+
+  ///歌曲所有歌单
+  static const String track_all = "assets/json/track_all.json";
+
+  ///歌单详情
+  static const String playlist_detail = "assets/json/playlist_detail.json";
+}
