118f9fa1d19a8c0190c6a58daa9a2008c984f477
niuxinhuai <niuxinhuai@super-niu-2.local>
2022-08-17 14:20:09 +0800

    排行榜type

--- lib/utils/typeUtils.dart
+++ lib/utils/typeUtils.dart
@@ -76,3 +76,210 @@ class HomeShowTypeUtils {
   ///可左右滑动，大的正方形
   static const String scroll_playList = "HOMEPAGE_SLIDE_PLAYLIST";
 }
+
+///排行榜
+class ToplistDetailType {
+  ///榜单推荐，编辑推荐榜
+  static const String top_compile = "编辑推荐榜VOL.11流行天后碧昂丝回归";
+
+  ///榜单推荐，说唱榜
+  static const String top_rap = "云音乐说唱榜";
+
+  ///榜单推荐，民谣榜
+  static const String top_ballad = "云音乐民谣榜";
+
+  ///官方榜单，飙升榜
+  static const String official_soar = "飙升榜";
+
+  ///官方榜单，新歌榜
+  static const String official_new = "新歌榜";
+
+  ///官方榜单，热歌榜
+  static const String official_hot = "热歌榜";
+
+  ///官方榜单，原创榜
+  static const String official_original = "原创榜";
+
+  ///精选榜单，说唱巅峰榜
+  static const String chosen_rap = "说唱巅峰榜";
+
+  ///精选榜单，BEAT排行榜
+  static const String chosen_beat = "BEAT排行榜";
+
+  ///精选榜单，网络热歌榜
+  static const String chosen_hot = "网络热歌榜";
+
+  ///精选榜单，黑胶VIP爱听榜
+  static const String chosen_vip = "黑胶VIP爱听榜";
+
+  ///曲风榜单，云音乐古典榜
+  static const String melody_classical = "云音乐古典榜";
+
+  ///曲风榜单，云音乐电音榜
+  static const String melody_electronic = "云音乐电音榜";
+
+  ///曲风榜单，云音乐ACG榜
+  static const String melody_acg = "云音乐ACG榜";
+
+  ///曲风榜单，云音乐国电榜
+  static const String melody_power = "云音乐国电榜";
+
+  ///曲风榜单，云音乐摇滚榜
+  static const String melody_rap = "云音乐摇滚榜";
+
+  ///曲风榜单，云音乐古风榜
+  static const String melody_antiquity = "云音乐古风榜";
+
+  ///曲风榜单，中文DJ榜
+  static const String melody_chinese_dj = "中文DJ榜";
+
+  ///全球榜单，俄罗斯top hit流行音乐榜
+  static const String world_russia = "俄罗斯top hit流行音乐榜";
+
+  ///全球榜单，法国 NRJ Vos Hits 周榜
+  static const String world_france = "法国 NRJ Vos Hits 周榜";
+
+  ///全球榜单，日本Oricon榜
+  static const String world_japan = "日本Oricon榜";
+
+  ///全球榜单，Beatport全球电子舞曲榜
+  static const String world_dj = "Beatport全球电子舞曲榜";
+
+  ///全球榜单，美国Billboard榜
+  static const String world_us = "美国Billboard榜";
+
+  ///全球榜单，UK排行榜周榜
+  static const String world_uk = "UK排行榜周榜";
+
+  ///语种榜单，Korean榜
+  static const String language_korean = "云音乐韩语榜";
+
+  ///语种榜单，云音乐欧美热歌榜
+  static const String language_us_hot = "云音乐欧美热歌榜";
+
+  ///语种榜单，云音乐欧美新歌榜
+  static const String language_us_new = "云音乐欧美新歌榜";
+
+  ///语种榜单，云音乐日语榜
+  static const String language_japan = "云音乐日语榜";
+
+  ///语种榜单，俄语榜
+  static const String language_russia = "俄语榜";
+
+  ///语种榜单，越南语榜
+  static const String language_vietnam = "越南语榜";
+
+  ///语种榜单，泰语榜
+  static const String language_thai = "泰语榜";
+
+  ///特色榜单，听歌识曲榜
+  static const String feature_listen = "听歌识曲榜";
+
+  ///特色榜单，潜力爆款榜
+  static const String feature_hot = "潜力爆款榜";
+
+  ///特色榜单，中国新乡村音乐排行榜
+  static const String feature_rural = "中国新乡村音乐排行榜";
+
+  ///特色榜单，KTV唛榜
+  static const String feature_ktv = "KTV唛榜";
+
+  ///ACG榜单，云音乐ACG VOCALOID榜
+  static const String acg_vocaloid = "云音乐ACG VOCALOID榜";
+
+  ///ACG榜单，云音乐ACG游戏榜
+  static const String acg_game = "云音乐ACG游戏榜";
+
+  ///ACG榜单，云音乐ACG动画榜
+  static const String acg_animation = "云音乐ACG动画榜";
+}
+
+class ToplistDetailUtils {
+  static const String top = "榜单推荐";
+
+  static const String official = "官方榜";
+
+  static const String chosen = "精选榜";
+
+  static const String melody = "曲风榜";
+
+  static const String world = "全球榜";
+
+  static const String language = "语种榜";
+
+//  static const String mv = "MV榜";
+
+  static const String feature = "特色榜";
+
+  static const String acg = "ACG榜";
+
+  ///榜单推荐
+  static List<String> top_list = [
+    ToplistDetailType.top_compile,
+    ToplistDetailType.top_rap,
+    ToplistDetailType.top_ballad
+  ];
+
+  ///官方榜单
+  static List<String> official_list = [
+    ToplistDetailType.official_soar,
+    ToplistDetailType.official_new,
+    ToplistDetailType.official_hot,
+    ToplistDetailType.official_original
+  ];
+
+  ///精选榜单
+  static List<String> chosen_list = [
+    ToplistDetailType.chosen_vip,
+    ToplistDetailType.chosen_rap,
+    ToplistDetailType.chosen_hot,
+    ToplistDetailType.chosen_beat
+  ];
+
+  ///曲风榜单
+  static List<String> melody_list = [
+    ToplistDetailType.melody_electronic,
+    ToplistDetailType.melody_acg,
+    ToplistDetailType.melody_rap,
+    ToplistDetailType.melody_power,
+    ToplistDetailType.melody_classical,
+    ToplistDetailType.melody_antiquity,
+    ToplistDetailType.melody_chinese_dj,
+  ];
+
+  ///全球榜
+  static List<String> world_list = [
+    ToplistDetailType.world_us,
+    ToplistDetailType.world_uk,
+    ToplistDetailType.world_japan,
+    ToplistDetailType.world_france,
+    ToplistDetailType.world_dj,
+    ToplistDetailType.world_russia
+  ];
+
+  ///语种榜
+  static List<String> language_list = [
+    ToplistDetailType.language_us_hot,
+    ToplistDetailType.language_us_new,
+    ToplistDetailType.language_japan,
+    ToplistDetailType.language_korean,
+    ToplistDetailType.language_russia,
+    ToplistDetailType.language_thai,
+    ToplistDetailType.language_vietnam,
+  ];
+
+  ///特色榜单
+  static List<String> feature_list = [
+    ToplistDetailType.feature_listen,
+    ToplistDetailType.feature_hot,
+    ToplistDetailType.feature_rural,
+    ToplistDetailType.feature_ktv
+  ];
+
+  ///ACG榜单
+  static List<String> acg_list = [
+    ToplistDetailType.acg_vocaloid,
+    ToplistDetailType.acg_game,
+    ToplistDetailType.acg_animation,
+  ];
+}
